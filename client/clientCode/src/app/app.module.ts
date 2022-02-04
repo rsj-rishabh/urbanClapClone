187f6ac5527a19c58dc5c98d1bca1186b1c349f7
RaghunandhanVV <vvrkaiser@gmail.com>
2022-02-04 17:24:17 -0500

    Fixed Bug #25
    Issue: Not able to see login and register after service list implementation
    
    Result: Fixed issue. Created separate component for services and added route from Login page

--- client/clientCode/src/app/app.module.ts
+++ client/clientCode/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
