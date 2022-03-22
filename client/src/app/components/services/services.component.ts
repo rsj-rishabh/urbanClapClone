68c6f092580a2f5d811eee1eca2444731e17c982
rsj-rishabh <rsj.rishabh@gmail.com>
2022-03-22 11:01:13 -0400

    fixed service API

--- client/src/app/components/services/services.component.ts
+++ client/src/app/components/services/services.component.ts
@@ -6,18 +6,20 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
+
 export class ServicesComponent implements OnInit {
 
   servicesData = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {      
-    // Simple GET request with response type <any>
-    this.http.get<any>('http://localhost:3000/api/services').subscribe(data => {
-        console.log('Hi')
+    this.http.get<any>('http://localhost:3000/api/getServices')
+      .subscribe(data => {
+        console.log(data)
         this.servicesData = data;
-    })
-}
+      }
+    )
+  }
 
 }
 
