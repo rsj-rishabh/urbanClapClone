f1641b3f820ca91e08fa11cf1bd92abd13a5950a
rsj-rishabh <rsj.rishabh@gmail.com>
2022-03-24 17:41:05 -0400

    added booking UI

--- client/src/app/components/service-booking/service-booking.component.ts
+++ client/src/app/components/service-booking/service-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-service-booking',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceBookingComponent implements OnInit {
 
+  bookingForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    gender : new FormControl(''),
+    username: new FormControl(''),
+    password : new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+
   constructor() { }
+
+  onSubmit() { }
 
   ngOnInit(): void {
   }
