d1caf547a853e0489c813cc0b1fc3d03036fbcf7
rsj-rishabh <rsj.rishabh@gmail.com>
2022-03-27 14:14:57 -0400

    fetched service data from routes

--- client/src/app/components/service-booking/service-booking.component.ts
+++ client/src/app/components/service-booking/service-booking.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-service-booking',
@@ -9,19 +11,34 @@ import { FormGroup,FormControl } from '@angular/forms';
 export class ServiceBookingComponent implements OnInit {
 
   bookingForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    gender : new FormControl(''),
-    username: new FormControl(''),
-    password : new FormControl(''),
-    confirmPassword: new FormControl('')
+    date: new FormControl(''),
+    time: new FormControl('')
   });
 
-  constructor() { }
+  constructor(private http: HttpClient,public router:Router,private route:ActivatedRoute) { }
 
-  onSubmit() { }
+  onSubmit() {
+    console.log(this.bookingForm.value)
+    if (this.bookingForm.value.date == '' || this.bookingForm.value.time == '') {
+      alert ('Please select appropriate date and time.')
+    } else {
+      var serviceId = 0
+      this.route.queryParams.subscribe(params => serviceId = params['service_id'])
 
-  ngOnInit(): void {
+      var endTime: number = +this.bookingForm.value.time.split(':')[0] + 1
+
+      var inpObject = {
+        'user_id': localStorage.getItem('id'),
+        'service_id': serviceId,
+        'date': this.bookingForm.value.date,
+        'start_time': this.bookingForm.value.time,
+        'end_time': endTime.toString() + ':' + this.bookingForm.value.time.split(':')[1]
+      }
+      console.log(inpObject)
+    }
+
   }
+
+  ngOnInit(): void { }
 
 }
