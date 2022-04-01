d7ccf57c22155e357d5b25104ced754b6ce3b839
pkgprateek <pkgprateek@gmail.com>
2022-04-01 15:22:02 -0400

    Booking Routing Error Fixed

---
+++ client/src/app/components/bookings/bookings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
