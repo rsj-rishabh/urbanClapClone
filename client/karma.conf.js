47da5aa24fff527543858efdde1f2f729c7049ae
rsj-rishabh <rsj.rishabh@gmail.com>
2022-04-17 19:10:56 +0530

    changed build paths

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/client-code'),
+      dir: require('path').join(__dirname, './coverage/client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clientCode'`, () => {
+  it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clientCode');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clientCode app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
   });
 });
